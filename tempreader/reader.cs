98dd0d5f2df89f1f73e354c7f117c1f9777ff931
Jeremy Morgan <jeremymorgan@gmail.com>
2015-07-28 09:36:37 -0700

    Update reader.cs
    cleaned up output for CSV

--- tempreader/reader.cs
+++ tempreader/reader.cs
@@ -1,25 +1,37 @@
 using System;
+using System.Text;
 
 namespace myReader {
 class Reader {	
+
 	public static void Main(){
-	
-		int counter = 0;
+
 		string line;
+		string temptab = "      \"temp\" : \"";
+		string datetab = "      \"date\" : \"";
 
 		System.IO.StreamReader file = new System.IO.StreamReader("bedroom.json");
+		int ctr = 0;
 		while((line = file.ReadLine()) != null)
 		{
-			if (line.Contains("temp")){
-				Console.WriteLine(line);
-			}
-	   
-		   counter++;
-		}
+			
+			if (line.Contains("temp") || line.Contains("date")){
 
+
+				StringBuilder b = new StringBuilder(line);
+				b.Replace(temptab, "");
+				b.Replace(datetab, "");
+				b.Replace("\"", "").Replace(",", "");
+
+				if (ctr % 2 ==0){
+					Console.Write(b + ",");
+				}else {
+					Console.WriteLine(b);
+				}
+				ctr++;
+			}	   
+		}
 		file.Close();
-
 		}
-
 	}
 }
